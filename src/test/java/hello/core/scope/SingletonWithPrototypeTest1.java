f707432c13681679603d750e4f13cb25158eaf5c
kejee1917 <kejee1917@gmail.com>
2026-02-05 18:02:56 +0900

    Section 10: Singleton With Prototype

---
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,73 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonWithPrototypeTest1 {
+    
+    @Test
+    void prototypeFind() {
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+
+    }
+
+    @Test
+    void singletonClientUseProto() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(2);
+    }
+
+    @Scope("singleton") //default라서 주석처리해도 됨
+    static class ClientBean {
+        private final PrototypeBean prototypeBean;
+
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+    
+    @Scope("prototype")
+    static class PrototypeBean{
+        private int count = 0;
+        
+        public void addCount() {
+            count++;
+        }
+        
+        public int getCount() {
+            return count;
+        }
+        
+        @PostConstruct
+        public void init() {
+            System.out.println("prototypeBean.init: " +this);
+        }
+        
+        @PreDestroy
+        public void destroy() {
+            System.out.println("prototypeBean.destroy"); // 호출 안됨
+        }
+    }
+}
