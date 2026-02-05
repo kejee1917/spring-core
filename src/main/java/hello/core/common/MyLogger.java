cdc8efd84a7e4918d953a4b48ef52ad50a0e3211
kejee1917 <kejee1917@gmail.com>
2026-02-05 18:51:03 +0900

    Section 10: request Scope

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,35 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Scope("request") // 요청시점에 생성, 요청 끝나는 시점에 소멸
+public class MyLogger {
+
+    private String uuid;
+    private String requestUrl;
+
+    public void setRequestUrl(String requestUrl) {
+        this.requestUrl = requestUrl;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestUrl + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "]" + "request scope bean create:" + this);
+    }
+
+    @PreDestroy // 고객 요청이 우리 서버에서 빠져나가면
+    public void close() {
+        System.out.println("[" + uuid + "]" + "request scope bean close:" + this);
+    }
+}
