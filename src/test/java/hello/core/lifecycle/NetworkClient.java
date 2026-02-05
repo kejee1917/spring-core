3c94f2da24425029426e544d973a9bbddd52876e
kejee1917 <kejee1917@gmail.com>
2026-02-05 14:40:55 +0900

    Section 9: @Bean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -31,15 +31,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 
 
     // 의존관계 주입이 끝나면!
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         connect();
         call("초기화 연결 메시지");
     }
 
     // 소멸
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         disconnect();
     }
 }
